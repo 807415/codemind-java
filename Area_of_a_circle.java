b9cd2e9b5bc599a68ef633023c3b612a911c4196
Chandrika <97424826+807415@users.noreply.github.com>
2022-10-14 15:13:55 +0530

    import java.util.*; class AreaCircle {     public static void main(String args[])     {
        double pi=3.14;
        Scanner sc=new Scanner(System.in);
        double r=sc.nextDouble();
        double area=pi*r*r;
        System.out.printf("%.2f",area);
        
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class AreaCircle
+{
+    public static void main(String args[])
+    {
+    
+    double pi=3.14;
+    Scanner sc=new Scanner(System.in);
+    double r=sc.nextDouble();
+    double area=pi*r*r;
+    System.out.printf("%.2f",area);
+    
+    }
+}
